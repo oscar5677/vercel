552b98e46deb2221ad303929943e41d6ff6b37aa
oscar5677 <72199362+oscar5677@users.noreply.github.com>
2025-04-10 21:28:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ $mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/' . $get;
 $mpdheads = [
     'http' => [
        // 'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+     'header' => "User-Agent: Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36",
+
         'follow_location' => 1,
         'timeout' => 5
     ]
